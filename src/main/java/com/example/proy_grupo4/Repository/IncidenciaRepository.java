b5a48d4278f8d63fab0571dad6eba54714862ec9
FrankLaquise <a20175947@pucp.edu.pe>
2022-09-20 16:21:10 -0500

    frank

--- src/main/java/com/example/proy_grupo4/Repository/IncidenciaRepository.java
+++ src/main/java/com/example/proy_grupo4/Repository/IncidenciaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IncidenciaRepository extends JpaRepository<Incidencia, Integer> {
+public interface IncidenciaRepository extends JpaRepository<Incidencia,Integer> {
 }
