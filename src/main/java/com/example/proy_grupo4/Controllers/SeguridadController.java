d7c4528ff125784dc9858d1ef179c7dff139c861
elias <m.yovera@pucp.edu.pe>
2022-09-20 19:29:18 -0500

    Seg_listar

--- src/main/java/com/example/proy_grupo4/Controllers/SeguridadController.java
+++ src/main/java/com/example/proy_grupo4/Controllers/SeguridadController.java
@@ -1,27 +1,44 @@
 package com.example.proy_grupo4.Controllers;
 
+import com.example.proy_grupo4.Entity.Incidencia;
+import com.example.proy_grupo4.Repository.IncidenciaRepository;
+import com.example.proy_grupo4.Repository.IncidenciaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/seguridad")
 public class SeguridadController {
-
+    @Autowired
+    IncidenciaRepository incidenciaRepository;
     @GetMapping(value = {"/login2F"})
     public String Seguridadlogin2F(){
         return "Seguridad_login2F";
     }
 
     @PostMapping(value = {"/inicio"})
-    public String SeguridadInicio(){
+    public String SeguridadInicio(Model model){
+        List<Incidencia> incidencias = incidenciaRepository.findAll();
+
         return "Seguridad_inicio";
     }
 
     @GetMapping(value = {"/incidencias"})
     public String Seguridadincidencias(){
         return "Seguridad_inicio";
+    }
+
+    @GetMapping(value = {"/incidenciaslistar"})
+    public String Seguridadincidenciaslistar(Model model){
+        List<Incidencia> lista = incidenciaRepository.findAll();
+        model.addAttribute("listaIncidencias", lista);
+        return "Seguridad_inicio2";
     }
 
     @GetMapping(value = {"/exportar"})
